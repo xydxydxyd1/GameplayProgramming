e3d815d4936d0859fe91956665239873e9fd12f9
Eric Xu <xydxydxyd1@gmail.com>
2024-01-18 16:13:06 -0800

    complete RollABall

--- Projects/Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Projects/Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -2,24 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    // Movement
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     public float speed = 0;
+
+    // PickUp
+    private int pickUpCount;
+    public TextMeshProUGUI pickUpCountText;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody>();
+        pickUpCount = 0;
+        SetPickUpCountText();
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
     }
 
     void OnMove (InputValue movementValue)
@@ -29,4 +37,18 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetPickUpCountText()
+    {
+        pickUpCountText.text = $"Count: {pickUpCount}";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            pickUpCount++;
+            SetPickUpCountText();
+        }
+    }
 }
